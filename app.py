8e85b63ffcde6c0d77285d1ad340ebd983c6a522
hgraphtech <71861726+hgraphtech@users.noreply.github.com>
2021-07-16 14:55:58 +1000

    update chart

--- app.py
+++ app.py
@@ -1,36 +1,52 @@
 from flask import Flask
 from flask import render_template
+import pandas as pd
+import datetime as dt
+
+
 
 app = Flask(__name__)
 
-dates = ['Jan','Feb','Mar','Apr','May','Jun','Jul']
 
-prices = [
-  {'commodity':'Oil', 'price_series':[47,53,61,60,65,67.5,75]},
-  {'commodity':'Gold', 'price_series':[1950,1860,1720,1730,1790,1905,1770]},
-  {'commodity':'Silver', 'price_series':[26.3,29,26.5,25,26.5,28.2,26]},
-  {'commodity':'Iron ore', 'price_series':[159,148,172,166,188,210,217]},
-  {'commodity':'Nickel', 'price_series':[16400,17600,18700,16200,18200,18100,18770]},
-  {'commodity':'Steel', 'price_series':[4200,4150,4600,4870,5300,4750,4800]},
-  {'commodity':'Soybeans', 'price_series':[1310,1365,1400,1400,1570,1550,1450]},
-  {'commodity':'Wheat', 'price_series':[640,645,640,610,725,690,650]}
-]
+df = pd.read_pickle('commod_price_data.pkl')
+df = df.fillna(method="ffill")
 
-data = [
-    {'commodity':'Oil', 'price':74.4, 'om':5, 'sm':53},
-    {'commodity':'Gold', 'price':1802, 'om':-3, 'sm':-5},
-    {'commodity':'Silver', 'price':26.0, 'om':-6, 'sm':-1},
-    {'commodity':'Iron ore', 'price':218, 'om':1, 'sm':39},
-    {'commodity':'Nickel', 'price':18770, 'om':1.4, 'sm':13},
-    {'commodity':'Steel', 'price':4917, 'om':2, 'sm':17},
-    {'commodity':'Soybeans', 'price':1404, 'om':-3, 'sm':9},
-    {'commodity':'Wheat', 'price':606, 'om':-7, 'sm':-2}
-]
+commodities = df.columns
+
+table_data = [{'commodity':c, 'price': round(df[c][-1],1),
+      'om': round(100*(df[c][-1]-df[c][-20])/df[c][-20],1),
+      'sm': round(100*(df[c][-1]-df[c][-120])/df[c][-120],1)}
+      for c in df.columns]
+
+dates = [d.strftime('%F')  for d in df.index]
+
+prices_data = [{'commodity':c, 'price_series':list(df[c].values)} for c in df.columns]
+
+chart_data = {}
+for c in commodities:
+  chart_data[c] = list(df[c].values)
+
+
 
 @app.route("/")
 def index():
-    return render_template('index.html', data=data)
+    return render_template('index.html', table_data=table_data)
 
+# @app.route("/<name>")
+# def hello(name):
+#   return render_template('index.html', table_data=table_data, commodity=name, chart_dates=dates, chart_data=chart_data[name])
+
+@app.route('/api/test', methods=['GET'])
+def test():
+  return {'data':[1,2,3,4,5]}
+
+@app.route('/api/table', methods=['GET'])
+def table():
+  return {'table_data':table_data}
+
+@app.route('/api/price', methods=['GET'])
+def chart():
+  return {'dates':dates, 'prices':prices_data}
 
 if __name__ == "__main__":
   app.run(debug=True)
--- static/dashboard.js
+++ static/dashboard.js
@@ -1,26 +1,20 @@
 
-// table data //
-
-
-let table = document.getElementById('pricetablebody');
-
 
 // charts //
 
-
 var ctx = document.getElementById('myChart')
-
 
 var myChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: dates,
+    labels: [],
     datasets: [{
-      data: prices[0].price_series,
+      data: [],
+      pointRadius: 0,
       lineTension: 0,
       backgroundColor: 'transparent',
       borderColor: '#007bff',
-      borderWidth: 4,
+      borderWidth: 2,
       pointBackgroundColor: '#007bff'
     }]
   },
@@ -39,11 +33,22 @@ var myChart = new Chart(ctx, {
 })
 
 
-function changedata() {
+
+function updatechart(data) {
   var c = $('#commod_chart').val();
-  myChart.data.datasets[0].data = prices[c].price_series;
+  myChart.data.labels = data.dates;
+  myChart.data.datasets[0].data = data.prices[c].price_series;
   myChart.update();
 }
 
+function changeData() {
+  fetch('/api/price')
+    .then(res => res.json())
+    .then(data => obj=data)
+    .then(() => updatechart(obj))
+}
 
-$('#commod_chart').on('change', changedata)
+
+changeData();
+
+$('#commod_chart').on('change', changeData)
